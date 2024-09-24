a26ff5cf08b5df55642c0a0f1f6856b011194d2a
David Peterson <peterdav000@warren.k12.in.us>
2024-09-24 13:20:20 -0400

    0000

--- src/java.java
+++ src/java.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class java {
+public class java {
+    //POJO:  Plain Old Java Object
+    private String make;
+    private String model;
+
+    public java(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.make = make;
+
+    }
+
 }
